aebc57e93f8f2dd6c1e8481ca0bb01e8a4273192
Vivian Miranda <vivianmiranda@email.arizona.edu>
2020-11-30 16:13:05 -0800

    update

--- source/supernovae.f90
+++ source/supernovae.f90
@@ -1,28 +1,27 @@
-    module snovae
+module snovae
     implicit none
-
     contains
-
     subroutine SNLikelihood_Add(LikeList, Ini)
-    use SNLS
-    use Union2
-    use likelihood
-    use settings
-    use JLA
-    use BBC
-    class(TLikelihoodList) :: LikeList
-    class(TSettingIni) :: ini
-    integer count
+        use SNLS
+        use Union2
+        use likelihood
+        use settings
+        use JLA
+        use BBC
+        class(TLikelihoodList) :: LikeList
+        class(TSettingIni) :: ini
+        integer count
 
-    if (.not. Ini%Read_Logical('use_SN',.false.)) return
-
-    count = LikeList%Count
-    call SNLSLikelihood_Add(LikeList, Ini)
-    call JLALikelihood_Add(LikeList, Ini)
-    call BBCLikelihood_Add(LikeList, Ini)
-    call Union2Likelihood_Add(LikeList, Ini)
-    if (LikeList%Count > count+1) call MpiStop('SNLikelihood_Add: more than one - datasets not independent')
-
+        if (.not. Ini%Read_Logical('use_SN',.false.)) then
+            return
+        endif
+        count = LikeList%Count
+        call SNLSLikelihood_Add(LikeList, Ini)
+        call JLALikelihood_Add(LikeList, Ini)
+        call BBCLikelihood_Add(LikeList, Ini)
+        call Union2Likelihood_Add(LikeList, Ini)
+        if (LikeList%Count > count+1) then
+            call MpiStop('SNLikelihood_Add: more than one - datasets not independent')
+        endif
     end subroutine SNLikelihood_Add
-
-    end module snovae
+end module snovae
--- source/supernovae_BBC.f90
+++ source/supernovae_BBC.f90
@@ -11,25 +11,26 @@ MODULE BBC
         procedure :: LogLikeTheory => BBC_LnLike
     end type BBCLikelihood
 
-    character(LEN=*), parameter :: BBC_version =  'December_2020'
-    REAL(mcp), PARAMETER, PRIVATE :: inv_twoPI = 1.0_mcp / twopi
     CHARACTER, PARAMETER, PRIVATE :: uplo = 'U' !For LAPACK
-    INTEGER, PARAMETER, PRIVATE :: snnamelen = 12
 
-    !Variables we will try to get from the ini file
-    CHARACTER(LEN=30), PRIVATE :: name !Name of data set
-    
     !Supernova data type
     TYPE, PRIVATE :: supernova
-        CHARACTER(LEN=snnamelen) :: name  !The name of the SN
-        REAL(mcp) :: zhel, zcmb   !The heliocentric and CMB frame redshifts
-        REAL(mcp) :: mag  !The K-corrected peak magnitude
+        CHARACTER(LEN=12) :: name     !The name of the SN
+        REAL(mcp) :: zhel             !The heliocentric frame redshift
+        REAL(mcp) :: zcmb             !The CMB frame redshift
+        REAL(mcp) :: mag              !Fitted m^b from JLA
+        REAL(mcp) :: mag_err
+        REAL(mcp) :: mu               !Distance Modulus (from BBC)
+        REAL(mcp) :: mu_err
     END TYPE supernova
 
     INTEGER, PUBLIC :: nsn  !Number of supernovae
     TYPE(supernova), ALLOCATABLE, PRIVATE :: sndata(:) !Supernova data
     REAL(mcp), ALLOCATABLE, PRIVATE :: covmat(:,:)
-    REAL(mcp), ALLOCATABLE, PRIVATE :: lumdists(:) !Other convenience variables
+
+    ! THEORY LUM DIST IS A GLOBAL MODULE VARIABLE TO AVOID
+    ! MALLOC/FREE AT EVERY CHAIN POINT
+    REAL(mcp), ALLOCATABLE, PRIVATE :: lumdists(:)
 
     LOGICAL, PUBLIC :: BBC_read = .FALSE.
     LOGICAL, PUBLIC :: BBC_prepped = .FALSE.
@@ -48,7 +49,7 @@ MODULE BBC
     !returning the number of lines in lines and the number of
     !non-comment lines in noncommentlines, where a comment line
     !is defined to start with a #. The file is rewound on exit
-    SUBROUTINE count_lines( lun, lines, noncommentlines )
+    SUBROUTINE count_lines(lun, lines, noncommentlines)
         IMPLICIT NONE
         INTEGER, INTENT(in) :: lun
         INTEGER, INTENT(out) :: lines, noncommentlines
@@ -83,70 +84,36 @@ MODULE BBC
     100 REWIND lun
     END SUBROUTINE count_lines
 
-    SUBROUTINE invert_covariance_matrix(invcovmat, status)
-        IMPLICIT NONE
-        INTEGER, INTENT(INOUT) :: status
-        REAL(mcp) :: invcovmat(:,:)
-
-        invcovmat = covmat
-        !Factor into Cholesky form, overwriting the input matrix
-        CALL DPOTRF(uplo,nsn,invcovmat,nsn,status)
-        IF ( status .NE. 0 ) THEN
-            STOP
-        END IF
-        !Note that DPOTRI only makes half of the matrix correct,
-        !so we have to be careful in what follows
-        CALL DPOTRI(uplo, nsn, invcovmat, nsn, status)
-        IF ( status .NE. 0 ) THEN
-            STOP
-        END IF
-    END SUBROUTINE invert_covariance_matrix
-
     !------------------------------------------------------------
     !------------------------------------------------------------
     !------------------------------------------------------------
     !------------------------------------------------------------
 
-    !Reads the covariance matrix from a file, given the filename
-    !and the number of elements to expect
-    !There are two possible formats supported
-    !These are: as one big block, and then as n by n individual elements
-    !The number of lines has to be the same as the number of SN, and
-    !they have to be in the same order. Copied from settings::ReadMatrix
+!------------------------------------------------------------
+! Reads the covariance matrix from a file, given the filename
+!------------------------------------------------------------
     SUBROUTINE read_cov_matrix(filename, mat, n)
         CHARACTER(LEN=*), INTENT(IN) :: filename
         INTEGER, INTENT(IN) :: n
         REAL(mcp), INTENT(OUT) :: mat(n,n)
-        INTEGER :: j,k, file_unit, nfile
+        INTEGER :: j, k, file_unit, nfile
         REAL(mcp) :: tmp
 
-        IF (Feedback > 2) WRITE(*,*) 'reading: '//trim(filename)
-        OPEN( newunit=file_unit, FILE=TRIM(filename), FORM='formatted', &
-            STATUS='old', ERR = 500 )
+        OPEN(newunit=file_unit, FILE=TRIM(filename), FORM='formatted', STATUS='old', ERR=500)
 
-        READ (file_unit, '(I5)', END=200, ERR=100) nfile
+        READ (file_unit, '(I5)', END=200, ERR=500) nfile
         IF (nfile /= n) THEN
-            WRITE (*,'("For file ",A," expected size ",I5," got ",I5)') &
-                TRIM(filename), n, nfile
+            WRITE (*,'("File ",A," expected size ",I5," got ",I5)') TRIM(filename), n, nfile
             STOP
         ENDIF
 
         DO j=1,n
-            READ (file_unit,*, end = 200, err=100) mat(j,1:n)
-        ENDDO
-
-        GOTO 120
-
-    100 REWIND(file_unit)  !Try other possible format
-        READ (file_unit, '(I5)', END=200, ERR=100) nfile
-
-        DO j=1,n
             DO k=1,n
-                READ (file_unit,*, end = 200) mat(j,k)
+                READ (file_unit, *, end=200) mat(j,k)
             END DO
         END DO
 
-    120 READ (file_unit,*, err = 150, end =150) tmp
+        READ (file_unit, *, err=150, end=150) tmp
         GOTO 200
 
     150 CLOSE(file_unit)
@@ -160,27 +127,21 @@ MODULE BBC
         STOP
     END SUBROUTINE read_cov_matrix
 
-    !------------------------------------------------------------
-    !Reads in a supernova data file, given knowledge of the number
-    !of lines to expect.  Ignores lines that start with #.
-    ! Input arguments:
-    !  lun              The lun number of the file to read.  Must be already open
-    !  nlines           The number of lines to expect in the file
-    !  nnoncommentlines The number of non-comment lines in the file
-    ! Output arguments:
-    !  sndata           The returned SN data, of length nnoncommentlines
-    !The file is not rewound on exit
-    !------------------------------------------------------------
-    SUBROUTINE read_BBC_data( lun, nlines, nnoncommentlines, sndata )
+!------------------------------------------------------------
+! Reads in a supernova data file, given knowledge of the
+! number of lines to expect. Ignores lines that start with #.
+!------------------------------------------------------------
+    SUBROUTINE read_BBC_data(lun, nlines, nnoncommentlines, sndata)
         IMPLICIT NONE
         INTEGER, INTENT(in) :: lun, nlines, nnoncommentlines
         TYPE(supernova), INTENT(out) :: sndata(nnoncommentlines)
         CHARACTER(LEN=80) :: inline, shiftline
         INTEGER:: i,count
         LOGICAL :: opened
+        REAL(mcp) :: tmp
         INTRINSIC ADJUSTL
 
-        INQUIRE( lun, OPENED=opened )
+        INQUIRE(lun, OPENED=opened)
         IF (.NOT. opened) THEN
             WRITE(*,*) "File is not open in count_lines"
             STOP
@@ -188,15 +149,18 @@ MODULE BBC
 
         count = 1
         DO i=1,nlines
-            !Read in line non-advancing
             READ (lun, '(A)', ERR = 20, END = 20) inline
-            shiftline = ADJUSTL( inline )
-            IF (shiftline(1:1) .EQ. '#') CYCLE
+            shiftline = ADJUSTL(inline)
+            IF (shiftline(1:1) .EQ. '#') THEN
+                CYCLE
+            ENDIF
 
     20      BACKSPACE lun
   
             READ (lun, *, ERR=60, END=50) sndata(count)%name, sndata(count)%zcmb, &
-                sndata(count)%zhel, sndata(count)%mag
+                sndata(count)%zhel, tmp, sndata(count)%mag, sndata(count)%mag_err
+            sndata(count)%mu = sndata(count)%mag + 19.3 ! TODO: OFFSET
+            sndata(count)%mu_err = sndata(count)%mag_err
             count = count+1
         END DO
 
@@ -207,11 +171,11 @@ MODULE BBC
 
     60  WRITE(*,*) 'Error reading in input data with: ',inline
         STOP
-
     END SUBROUTINE read_BBC_data
 
     SUBROUTINE BBC_prep
         IMPLICIT NONE
+        integer :: j, k, status
 
         IF (.NOT. BBC_read) THEN
             STOP 'BBC data was not read in'
@@ -222,14 +186,33 @@ MODULE BBC
         
         ALLOCATE(lumdists(nsn))
 
+        DO j=1,nsn
+            covmat(j,j) = covmat(j,j) + sndata(j)%mu_err*sndata(j)%mu_err
+        END DO
+
+        !Factor into Cholesky form, overwriting the input matrix
+        CALL DPOTRF(uplo, nsn, covmat, nsn, status)
+        IF (status .NE. 0) THEN
+            STOP
+        END IF
+        !Note that DPOTRI only makes half of the matrix correct,
+        !so we have to be careful in what follows
+        CALL DPOTRI(uplo, nsn, covmat, nsn, status)
+        IF (status .NE. 0) THEN
+            STOP
+        END IF
+
+        DO k=1,nsn
+            DO j=k+1,nsn
+                covmat(j,k) = covmat(k,j)
+            ENDDO
+        ENDDO
+        
         BBC_prepped = .TRUE.
-
         RETURN
-
-        STOP
     END SUBROUTINE BBC_prep
 
-    SUBROUTINE read_BBC_dataset( filename )
+    SUBROUTINE read_BBC_dataset(filename)
         IMPLICIT NONE
         CHARACTER(LEN=*), INTENT(in) :: filename
         CHARACTER(LEN=:), allocatable :: covfile
@@ -242,20 +225,19 @@ MODULE BBC
             STOP 'Error -- BBC data already read'
         ENDIF
 
-        !Process the Ini file
         CALL Ini%Open(filename)
-        name = Ini%Read_String('name', .FALSE.)
-        data_file = Ini%Read_String_Default('data_file', trim(DataDir)//'BBC_lcparams.txt')
-        !Now read the actual SN data
-        OPEN(newunit=file_unit, FILE=TRIM(data_file), FORM='formatted', STATUS='old', ERR = 500)
+        data_file = Ini%Read_String('data_file')
+        
+        OPEN(newunit=file_unit, FILE=TRIM(data_file), FORM='formatted', STATUS='old', ERR=500)
         CALL count_lines(file_unit, nlines, nsn) !Find the number of lines
+
         ALLOCATE(sndata(nsn))
         CALL read_BBC_data(file_unit, nlines, nsn, sndata)
         CLOSE(file_unit)
 
         covfile = Ini%Read_String('covmat_file', .TRUE.)
         ALLOCATE(covmat(nsn, nsn))
-        CALL read_cov_matrix( covfile, covmat, nsn )
+        CALL read_cov_matrix(covfile, covmat, nsn)
         CALL Ini%Close()
 
         BBC_read = .TRUE.
@@ -281,13 +263,12 @@ MODULE BBC
         endif
 
         allocate(this)
-        this%LikelihoodType = 'SN'
         this%name='BBC'
+        this%LikelihoodType = 'SN'
         this%needs_background_functions = .true.
-        this%version = Ini%Read_String_Default('BBC_version',BBC_version)
-        call this%loadParamNames(trim(DataDir)//'BBC.paramnames')
+        this%version = Ini%Read_String('BBC_version')
         call LikeList%Add(this)
-        BBC_filename = Ini%Read_String_Default('BBC_dataset',trim(DataDir)//'BBC.dataset')
+        BBC_filename = Ini%Read_String('BBC_dataset')
         CALL read_BBC_dataset(BBC_filename)
         CALL BBC_prep
     end subroutine BBCLikelihood_Add
@@ -295,9 +276,8 @@ MODULE BBC
     FUNCTION BBC_LnLike(this, CMB)
         Class(BBCLikelihood) :: this
         Class(CMBParams) CMB
-        REAL(mcp) :: BBC_LnLike
-        real(mcp) zhel, zcmb
-        integer i
+        real(mcp) zhel, zcmb, dl, BBC_LnLike, BBC_CHI2
+        integer i, j
 
         BBC_LnLike = logZero
 
@@ -309,14 +289,21 @@ MODULE BBC
             STOP 'BBC data not prepped; run BBC_prep'
         ENDIF
 
-        !Get the luminosity distances.
         DO i=1,nsn
             zhel = sndata(i)%zhel
             zcmb = sndata(i)%zcmb
-            lumdists(i) = 5.0*LOG10((1.0+zhel)*(1.0+zcmb)*this%Calculator%AngularDiameterDistance(zcmb))
+            dl = (1.0+zhel)*(1.0+zcmb)*this%Calculator%AngularDiameterDistance(zcmb)
+            lumdists(i) = 5.0*LOG10(dl/1e-5)
         ENDDO
 
-        BBC_LnLike=0.0
+        BBC_CHI2=0.0
+        DO i=1,nsn
+            DO j=1,nsn
+                BBC_CHI2 = BBC_CHI2 + (lumdists(i)-sndata(i)%mu)*covmat(i,j)*(lumdists(j)-sndata(j)%mu)                
+            ENDDO
+        ENDDO
+        ! TODO: H0 MARGINALIZATION
+        BBC_LnLike = 0.5*BBC_CHI2
     END FUNCTION BBC_LnLike
 
 END MODULE BBC
